8949bc938f989c892a316c2fb785baf5c77ed9fc
zoel-a <zulmyazhary32@gmail.com>
2024-03-21 17:24:49 +0800

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Navbar } from "@/components/layout/navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1 className="font-medium">About Us</h1>
+    </div>
+  );
+}
+
+export default App;
