8e87173641f614a287cb3f98cb7851c0c7b91fb0
zoel-a <zulmyazhary32@gmail.com>
2024-03-23 05:54:46 +0800

    feat: add button variant

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,18 +1,28 @@
 import { cn } from "@/lib/utils";
+import { cva, type VariantProps } from "class-variance-authority";
 
-type ButtonProps = React.ComponentProps<"button">;
+const buttonVariants = cva(
+  "w-fit px-8 py-3 text-sm font-bold tracking-wider uppercase rounded-lg transition-colors",
+  {
+    variants: {
+      variant: {
+        primary: "bg-black text-white hover:bg-black/90",
+        secondary: "bg-white text-black hover:bg-white/90",
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+    },
+  }
+);
+
+type ButtonProps = React.ComponentProps<"button"> & VariantProps<typeof buttonVariants>;
 
 export const Button: React.FC<ButtonProps> = (props) => {
-  const { className, children, ...rest } = props;
+  const { variant, className, children, ...rest } = props;
 
   return (
-    <button
-      {...rest}
-      className={cn(
-        "w-fit px-8 py-3 text-sm font-bold tracking-wider text-white uppercase bg-black rounded-lg",
-        className
-      )}
-    >
+    <button className={cn(buttonVariants({ className, variant }))} {...rest}>
       {children}
     </button>
   );
