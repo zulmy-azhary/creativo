957aeb3b98c8209f616b88dc25c10fc62adcb556
zoel-a <zulmyazhary32@gmail.com>
2024-03-23 04:46:58 +0800

    refactor: update services' section

--- src/components/layout/services.tsx
+++ src/components/layout/services.tsx
@@ -1,5 +1,14 @@
+import { Accordion } from "@/components/ui/accordion";
+
 export const Services: React.FC = () => {
   return (
-    <div className="bg-black w-full h-[500px] text-white">Services</div>
-  )
-}
+    <section className="flex justify-center w-full text-white bg-black">
+      <div className="container flex flex-col my-24 2xl:gap-x-7 2xl:flex-row px-7 md:px-7 lg:px-28 xl:my-52 gap-y-2">
+        <h2 className="text-xl 2xl:translate-y-8 basis-1/6">Our Services</h2>
+        <div className="w-full">
+          <Accordion />
+        </div>
+      </div>
+    </section>
+  );
+};
