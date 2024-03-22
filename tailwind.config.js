0cf6647244e3cd2952b0e351cd775471bffa5ebb
zoel-a <zulmyazhary32@gmail.com>
2024-03-22 16:04:00 +0800

    chore: modify default breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+      "2xl": "1728px",
+      "3xl": "2560px",
+    },
   },
   plugins: [],
 };
-
