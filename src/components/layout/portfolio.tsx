e9ce0713bbc147709e5cecfec9e6b4774150f4d2
zoel-a <zulmyazhary32@gmail.com>
2024-03-23 05:46:41 +0800

    style: change margin and flex gap

--- src/components/layout/portfolio.tsx
+++ src/components/layout/portfolio.tsx
@@ -1,7 +1,7 @@
 export const Portfolio: React.FC = () => {
   return (
     <section className="flex justify-center w-full">
-      <div className="container flex flex-col my-24 2xl:gap-x-7 2xl:flex-row px-7 md:px-7 lg:mb-0 lg:px-28 xl:my-52 gap-y-2">
+      <div className="container flex flex-col my-24 2xl:gap-x-7 2xl:flex-row px-7 md:px-7 lg:px-28 xl:my-52 gap-y-7">
         <h2 className="text-xl 2xl:translate-y-4 basis-1/6">Our Portfolio</h2>
         <div className="flex flex-col w-full gap-y-28">
           <h2 className="text-4xl font-medium leading-relaxed lg:font-semibold lg:tracking-wide lg:max-w-5xl">
