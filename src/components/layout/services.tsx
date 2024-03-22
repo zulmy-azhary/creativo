38641498bb568599e8d9ca7810ec5c8f5fb053cf
zoel-a <zulmyazhary32@gmail.com>
2024-03-23 06:03:51 +0800

    style: remove redundant padding

--- src/components/layout/services.tsx
+++ src/components/layout/services.tsx
@@ -3,7 +3,7 @@ import { Accordion } from "@/components/ui/accordion";
 export const Services: React.FC = () => {
   return (
     <section className="flex justify-center w-full text-white bg-black">
-      <div className="container flex flex-col my-24 2xl:gap-x-7 2xl:flex-row px-7 md:px-7 lg:px-28 xl:my-52 gap-y-2">
+      <div className="container flex flex-col my-24 2xl:gap-x-7 2xl:flex-row px-7 lg:px-28 xl:my-52 gap-y-2">
         <h2 className="text-xl 2xl:translate-y-8 basis-1/6">Our Services</h2>
         <div className="w-full">
           <Accordion />
