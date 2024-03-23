6671593b76dd1668819aaff524740ba362da25d5
zoel-a <zulmyazhary32@gmail.com>
2024-03-24 05:03:37 +0800

    feat: create footer navigation list component

---
+++ src/components/ui/nav-list.tsx
@@ -0,0 +1,27 @@
+import { NavItem } from "@/components/ui/nav-item";
+
+type NavListProps = {
+  title: string;
+  items: { href?: string; content: string }[];
+};
+
+export const NavList: React.FC<NavListProps> = (props) => {
+  const { title, items } = props;
+
+  return (
+    <div className="space-y-14">
+      <h3 className="text-xl font-semibold">{title}</h3>
+      <ul className="flex flex-col gap-y-10">
+        {items.map(({ href, content }) => (
+          <NavItem
+            key={content}
+            href={href ?? "#"}
+            className="font-light tracking-tight hover:font-light md:tracking-wider text-white/50 hover:text-white"
+          >
+            {content}
+          </NavItem>
+        ))}
+      </ul>
+    </div>
+  );
+};
