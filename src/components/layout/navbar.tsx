5acada36f24144d159f6986f25001e1b4296a79e
zoel-a <zulmyazhary32@gmail.com>
2024-03-24 16:47:40 +0800

    feat: implement nav-control component

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,9 +1,10 @@
 import { NavItem } from "@/components/ui/nav-item";
 import { useMediaQuery } from "@/hooks/media-query";
-import { RxHamburgerMenu } from "react-icons/rx";
 import { Button } from "@/components/ui/button";
 import { Logo } from "@/components/ui/logo";
 import { Reveal } from "@/components/utils/reveal";
+import { NavControl } from "@/components/ui/nav-control";
+import { ToggleProvider } from "@/context/toggle";
 
 export const Navbar: React.FC = () => {
   const isLaptop = useMediaQuery("(min-width: 1024px)");
@@ -13,26 +14,24 @@ export const Navbar: React.FC = () => {
       <Reveal from="left">
         <Logo />
       </Reveal>
-      {isLaptop ? (
-        <nav className="flex items-center list-none gap-x-12">
-          <Reveal delay={0.25}>
-            <NavItem href="#">Home</NavItem>
-          </Reveal>
-          <Reveal delay={0.4}>
-            <NavItem href="#">About Us</NavItem>
-          </Reveal>
-          <Reveal delay={0.55}>
-            <NavItem href="#">Portfolio</NavItem>
-          </Reveal>
-          <Reveal delay={0.7}>
-            <Button>Get In Touch</Button>
-          </Reveal>
-        </nav>
-      ) : (
-        <button type="button" className="p-2 rounded hover:bg-black/5">
-          <RxHamburgerMenu className="size-6" />
-        </button>
-      )}
+      <ToggleProvider>
+        <NavControl isLaptop={isLaptop}>
+          <nav className="flex flex-col items-center list-none lg:flex-row gap-x-12 gap-y-14">
+            <Reveal delay={0.25}>
+              <NavItem href="#">Home</NavItem>
+            </Reveal>
+            <Reveal delay={0.4}>
+              <NavItem href="#">About Us</NavItem>
+            </Reveal>
+            <Reveal delay={0.55}>
+              <NavItem href="#">Portfolio</NavItem>
+            </Reveal>
+            <Reveal delay={0.7}>
+              <Button variant={isLaptop ? "primary" : "secondary"}>Get In Touch</Button>
+            </Reveal>
+          </nav>
+        </NavControl>
+      </ToggleProvider>
     </header>
   );
 };
